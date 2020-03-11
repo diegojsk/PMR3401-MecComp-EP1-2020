85364560557e12ee47147dc7b6497a8a15dbaf32
diegojsk <diego.jun@thunderatz.org>
2020-03-11 16:07:22 -0300

    Fix array/matrix problem fo methods

--- euler_method.m
+++ euler_method.m
@@ -7,12 +7,12 @@ function Y = euler_method(f,h,yi,xi,xf)
 %     
 %% Algorithm
 i = 1;
-y(i) = yi;
-x(i) = xi;
+y(:,i) = yi;
+x(:,i) = xi;
 steps = (xf-xi)/h;
 for i=1:steps
-    y(i+1) = y(i) + h*f(x(i),y(i));
-    x(i+1) = x(i) + h;
+    y(:,i+1) = y(:,i) + h*f(x(:,i),y(:,i));
+    x(:,i+1) = x(:,i) + h;
 end
  Y =  y;  
 end
--- runge_kutta_2.m
+++ runge_kutta_2.m
@@ -3,14 +3,14 @@ function Y = runge_kutta_2(f,h,yi,xi,xf)
 %
 %% Algorithm
 i = 1;
-y(i) = yi;
-x(i) = xi;
+y(:,i) = yi;
+x(:,i) = xi;
 steps = (xf-xi)/h;
 for i=1:steps
-    k1 = f(x(i),y(i));
-    k2 = f(x(i) + h*(1/2), y(i) +h*k1*(1/2) );
-    y(i+1) = y(i) + h*k2;
-    x(i+1) = x(i) + h;
+    k1 = f(x(:,i),y(:,i));
+    k2 = f(x(:,i) + h*(1/2), y(i) +h*k1*(1/2) );
+    y(:,i+1) = y(:,i) + h*k2;
+    x(:,i+1) = x(:,i) + h;
 end
  Y =  y;  
 
