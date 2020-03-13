a81573153e03fd6e08257ea453fb6b54330f2613
diegojsk <diego.jun@thunderatz.org>
2020-03-12 22:29:37 -0300

    Add system diff equation

--- main.m
+++ main.m
@@ -8,10 +8,10 @@
 % Testar Execução dos Métodos Numéricos: 
 % y' = 2x => y = x^2
 
-f = @(x,y) 2*x;
-euler_method(f,1,1,1,5)
-runge_kutta_2(f,1,1,1,5)
-runge_kutta_4(f,1,1,1,5)
+t = @(x,y) 2*x;
+euler_method(t,1,1,1,5);
+runge_kutta_2(t,1,1,1,5);
+runge_kutta_4(t,1,1,1,5);
 
 %% Equação do Modelo
 %
@@ -26,7 +26,7 @@ F2 = -0.5;
 uIz = 2.7;
 R = 0.3;
 g = 9.81;
-veld = 80;
+veld = 80/3.6;
 
 % Equações 
 %
@@ -37,9 +37,32 @@ veld = 80;
 % thetha2' = y(4)
 
 % Equação de Movimento
-f = @(t,y) [y(2);
-            y(2)*2;
-            y(3);
-            y(4) + y(1)];
+
+% A0 = ((L1^2)*L2*R*(m2*cos(2*y(1) - 2*y(3)) - 2*m1 - m2));
+% A1 = ((L1^2)*L2*R*m2*sin(2*y(1) - 2*y(3)));
+% A2 = (2*L1*(L2^2)*R*m2*sin(y(1)-y(3)));
+% A3 = (-2*L2*uIz*veld);
+% A4 = (-2*L1*uIz*veld*cos(y(1) - y(3)));
+% A5 = (-R*L1*( L2eixo*F2*sin(y(1) - 2*y(3)) + 2*sin(y(1))*( F1*L2 + (1/2)*L2eixo*F2)));
+
+% B0 = ((L2^2)*R*m2);
+% B1 = (-L1*L2*R*m2*cos(y(1) - y(3)));
+% B2 = (L1*L2*R*m2*sin(y(1) - y(3)));
+% B3 = (-uIz*veld);
+% B4 = (L2eixo*sin(y(3))*R*F2);
+
+
+% f = @(t,y) [ y(2);
+%             (1/A0)*( A1*(y(2)^2) + A2*(y(4)^2) + A3*y(2) + A4*y(2) + A5);
+%              y(3);
+%            ((1/B0)*( B1*((1/A0)*( A1*(y(2)^2) + A2*(y(4)^2) + A3*y(2) + A4*y(2)+ A5)) + B2*(y(2)^2) + B3*y(4) + B4 ))];
+           
+f = @(t,y) [ y(2);
+            (1/((L1^2)*L2*R*(m2*cos(2*y(1) - 2*y(3)) - 2*m1 - m2)))*(  ((L1^2)*L2*R*m2*sin(2*y(1) - 2*y(3)))*(y(2)^2) + (2*L1*(L2^2)*R*m2*sin(y(1)-y(3)))*(y(4)^2) + (-2*L2*uIz*veld)*y(2) + (-2*L1*uIz*veld*cos(y(1) - y(3)))*y(2) + (-R*L1*( L2eixo*F2*sin(y(1) - 2*y(3)) + 2*sin(y(1))*( F1*L2 + (1/2)*L2eixo*F2))));
+             y(3);
+           ((1/((L2^2)*R*m2))*( (-L1*L2*R*m2*cos(y(1) - y(3)))*((1/((L1^2)*L2*R*(m2*cos(2*y(1) - 2*y(3)) - 2*m1 - m2)))*( ((L1^2)*L2*R*m2*sin(2*y(1) - 2*y(3)))*(y(2)^2) + (2*L1*(L2^2)*R*m2*sin(y(1)-y(3)))*(y(4)^2) + (-2*L2*uIz*veld)*y(2) + (-2*L1*uIz*veld*cos(y(1) - y(3)))*y(2) + (-R*L1*( L2eixo*F2*sin(y(1) - 2*y(3)) + 2*sin(y(1))*( F1*L2 + (1/2)*L2eixo*F2))))) + (L1*L2*R*m2*sin(y(1) - y(3)))*(y(2)^2) + (-uIz*veld)*y(4) + (L2eixo*sin(y(3))*R*F2) ))];
+           
         
-euler_method(f,1,[1; 70; 3; 1/5],0,5)
+euler_method(f,1,[0; 0.4; 0; -0.5],0,60)
+runge_kutta_2(f,1,[0; 0.4; 0; -0.5],0,60)
+runge_kutta_4(f,1,[0; 0.4; 0; -0.5],0,60)
