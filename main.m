498c7d2d001504841bf33f88bb2baebe7b4625c2
diegojsk <diego.jun@thunderatz.org>
2020-03-13 06:28:18 -0300

    Fix runge_kutta_2 index reference

--- main.m
+++ main.m
@@ -8,10 +8,10 @@
 % Testar Execução dos Métodos Numéricos: 
 % y' = 2x => y = x^2
 
-t = @(x,y) 2*x;
-euler_method(t,1,1,1,5);
-runge_kutta_2(t,1,1,1,5);
-runge_kutta_4(t,1,1,1,5);
+%t = @(x,y) 2*x;
+%euler_method(t,1,1,1,5);
+%runge_kutta_2(t,1,1,1,5);
+%runge_kutta_4(t,1,1,1,5);
 
 %% Equação do Modelo
 %
@@ -63,6 +63,6 @@ f = @(t,y) [ y(2);
            ((1/((L2^2)*R*m2))*( (-L1*L2*R*m2*cos(y(1) - y(3)))*((1/((L1^2)*L2*R*(m2*cos(2*y(1) - 2*y(3)) - 2*m1 - m2)))*( ((L1^2)*L2*R*m2*sin(2*y(1) - 2*y(3)))*(y(2)^2) + (2*L1*(L2^2)*R*m2*sin(y(1)-y(3)))*(y(4)^2) + (-2*L2*uIz*veld)*y(2) + (-2*L1*uIz*veld*cos(y(1) - y(3)))*y(2) + (-R*L1*( L2eixo*F2*sin(y(1) - 2*y(3)) + 2*sin(y(1))*( F1*L2 + (1/2)*L2eixo*F2))))) + (L1*L2*R*m2*sin(y(1) - y(3)))*(y(2)^2) + (-uIz*veld)*y(4) + (L2eixo*sin(y(3))*R*F2) ))];
            
         
-euler_method(f,1,[0; 0.4; 0; -0.5],0,60)
-runge_kutta_2(f,1,[0; 0.4; 0; -0.5],0,60)
-runge_kutta_4(f,1,[0; 0.4; 0; -0.5],0,60)
+euler_method(f,0.1,[0; 0.4; 0; -0.1],0,60);
+runge_kutta_2(f,0.1,[0; 0.4; 0; -0.1],0,60);
+runge_kutta_4(f,0.1,[0; 0.4; 0; -0.1],0,60);
--- runge_kutta_2.m
+++ runge_kutta_2.m
@@ -8,10 +8,10 @@ x(:,i) = xi;
 steps = (xf-xi)/h;
 for i=1:steps
     k1 = f(x(:,i),y(:,i));
-    k2 = f(x(:,i) + h*(1/2), y(i) +h*k1*(1/2) );
+    k2 = f(x(:,i) + h*(1/2), y(:,i) +h*k1*(1/2) );
     y(:,i+1) = y(:,i) + h*k2;
     x(:,i+1) = x(:,i) + h;
 end
- Y =  y;  
+ Y = y;  
 
 end
